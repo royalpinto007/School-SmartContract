b31d97dc810e0aae18423e4bd865f4df6a7588cd
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-18 10:02:30 +0530

    Update School.sol

--- School.sol
+++ School.sol
@@ -1,21 +1,25 @@
-pragma solidity >= 0.7.0<0.8.0;
+pragma solidity ^0.6.6;
   
 contract School
 {
-    struct Student
+    
+    address public owner;
+    uint public mostSent;
+    address public richest;
+    
+   struct Student
     {   
-        int rollNumber;
-        int regnumber;
+        int256 rollNumber; // use int256 in place of int
+        int256 regnumber;
         string fName;
         string lName;
-        int dob;
-        int marks;
-	 
+        string dob; // dob is in string since we have to use the - symbol too which is not a int
+        int256 marks;
     }
   
-    address owner;
     int public stdCount = 0;
-    mapping(int => Student) public stdRecords;
+    //mapping(int => Student) public stdRecords;
+    Student[] public stdRecords; // stdRecords must be an array as we have to push the details in it
   
     modifier onlyOwner
     {
@@ -23,23 +27,51 @@ contract School
         _;
     }
 
-    constructor()
+    constructor() public
     {
         owner=msg.sender;
     }
-  
-    
-    function addNewRecords(int _rollNumber,
-						  int _regnumber,
-                          string memory _fName,
-                          string memory _lName,
-						  int _dob, 
-                          int _marks) public onlyOwner
+   
+    mapping (address => uint) pendingWithdraws;
+
+
     {
         
-        stdCount = stdCount + 1;  // to fetch details of students, I have used count since i didnt understand how to do it using regnumber)
-        
-        stdRecords[stdCount] = Student(_rollNumber, _regnumber, _fName, _lName, _dob, _marks);
+        stdCount = stdCount + 1;  	// to fetch details of students, I have used count since i didnt understand how to do it using regnumber)
+        //stdRecords[stdCount] = Student(_rollNumber, _regnumber, _fName, _lName, _dob, _marks);
+        stdRecords.push(Student({rollNumber:_rollNumber, regnumber:_regnumber, fName:_fName, lName:_lName, dob:_dob, marks:_marks}));
     }
   
+  	function() external payable {
+        owner.transfer(msg.value); 			// to transfer funds to owner account
+    }
+    
+      function School () payable {
+        richest = msg.sender;
+        mostSent = msg.value;
+        owner = msg.sender;
+    }
+    
+    function addNewRecords(int256 _rollNumber,
+			   int256 _regnumber,
+                          string memory _fName,
+                          string memory _lName,
+			  string memory _dob, 
+                          int256 _marks) public 
+		  
+    function becomeRichest() payable returns (bool){
+        require(msg.value > mostSent);
+        pendingWithdraws[richest] += msg.value;
+        richest = msg.sender;
+        mostSent = msg.value;
+        return true;
+    }
+
+    function withdraw(uint amount) onlyOwner returns(bool) {
+        // uint amount = pendingWithdraws[msg.sender];
+        // pendingWithdraws[msg.sender] = 0;
+        // msg.sender.transfer(amount);
+        require(amount < this.balance);
+        owner.transfer(amount);
+        return true;
 }
